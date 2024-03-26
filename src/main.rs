1d9dd7a0686bb996307ff67ac37a002c2462a055
Tomruler <bricechen101@gmail.com>
2024-03-26 11:51:44 -0500

    Separated runtime from connection, can now get UI to trigger vibration events.

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,12 @@ use buttplug::{
       message::{ClientGenericDeviceMessageAttributes},
     },
   };
-
+// use buttplug::core::connector::ButtplugConnectorError;
 
 pub struct BPIntifaceClient
 {
-  placeholder_bool: bool,
-  rt: Runtime,
+  client: Option<ButtplugClient>,
+  rt: Option<Runtime>,
 }
 
 impl BPIntifaceClient
@@ -30,8 +30,21 @@ impl BPIntifaceClient
         .unwrap();
 
     // Call the asynchronous connect method using the runtime.
-    let inner = rt.block_on(test_buttplug());
+    let _inner = rt.block_on(test_buttplug());
   } 
+  pub fn connect(&mut self){
+    self.rt = Some(tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap());
+
+    // Call the asynchronous connect method using the runtime.
+    self.client = Some(self.rt.as_mut().unwrap().block_on(connect_buttplug()).unwrap());
+  }
+  pub fn vibrate(&mut self)
+  {
+    self.rt.as_mut().unwrap().block_on(vibrate_buttplug(&self.client.as_mut().unwrap()));
+  }
 }
 
 fn main() -> Result<(), eframe::Error> {
@@ -45,7 +58,8 @@ fn main() -> Result<(), eframe::Error> {
     // let rt = tokio::runtime::Builder::new_current_thread().enable_all().build();
     // let inner = rt.block_on(test_buttplug());
     BPIntifaceClient::test();
-
+    // let bp_client = BPIntifaceClient;
+    // bp_client.connect();
     eframe::run_native(
         "My egui App",
         options,
@@ -54,6 +68,7 @@ fn main() -> Result<(), eframe::Error> {
             egui_extras::install_image_loaders(&cc.egui_ctx);
 
             Box::<MyApp>::default()
+            // Box::<MyApp>::bound(bp_client)
         }),
     )
 }
@@ -61,6 +76,7 @@ fn main() -> Result<(), eframe::Error> {
 struct MyApp {
     name: String,
     age: u32,
+    bp_client: Option<BPIntifaceClient>,
 }
 
 impl Default for MyApp {
@@ -68,8 +84,17 @@ impl Default for MyApp {
         Self {
             name: "Arthur".to_owned(),
             age: 42,
+            bp_client: None,
         }
     }
+
+    // fn bound(client: BPIntifaceClient) -> Self {
+    //   Self {
+    //       name: "Arthur".to_owned(),
+    //       age: 42,
+    //       bp_client: client,
+    //   }
+    // }
 }
 
 impl eframe::App for MyApp {
@@ -84,7 +109,13 @@ impl eframe::App for MyApp {
             ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
             if ui.button("Increment").clicked() {
                 self.age += 1;
-                BPIntifaceClient::test();
+                self.bp_client.as_mut().unwrap().vibrate();
+            }
+            if ui.button("Connect").clicked() {
+              self.bp_client = Some(BPIntifaceClient{
+                client:None, rt:None
+              });
+              self.bp_client.as_mut().unwrap().connect();
             }
             ui.label(format!("Hello '{}', age {}", self.name, self.age));
 
@@ -193,4 +224,107 @@ async fn test_buttplug() -> anyhow::Result<()> {
       println!("{}", error);
     }
     Ok(())
+}
+
+async fn connect_buttplug() -> Result<ButtplugClient, ButtplugClientError>
+{
+    println!("Attempting Connection");
+    let connector = new_json_ws_client_connector("ws://localhost:12345");
+  
+    let client = ButtplugClient::new("Example Client");
+    client.connect(connector).await?;
+  
+    println!("Connected!");
+  
+    // You usually shouldn't run Start/Stop scanning back-to-back like
+    // this, but with TestDevice we know our device will be found when we
+    // call StartScanning, so we can get away with it.
+    client.start_scanning().await?;
+    client.stop_scanning().await?;
+    println!("Client currently knows about these devices:");
+    for device in client.devices() {
+      println!("- {}", device.name());
+    }
+    // wait_for_input().await;
+  
+    for device in client.devices() {
+      fn print_attrs(attrs: &Vec<ClientGenericDeviceMessageAttributes>) {
+        for attr in attrs {
+          println!(
+            "{}: {} - Steps: {}",
+            attr.actuator_type(),
+            attr.feature_descriptor(),
+            attr.step_count()
+          );
+        }
+      }
+      println!("{} supports these actions:", device.name());
+      if let Some(attrs) = device.message_attributes().scalar_cmd() {
+        print_attrs(attrs);
+      }
+      print_attrs(&device.rotate_attributes());
+      print_attrs(&device.linear_attributes());
+      println!("Battery: {}", device.has_battery_level());
+      println!("RSSI: {}", device.has_rssi_level());
+    }
+    Ok(client)
+}
+
+async fn vibrate_buttplug(client: &ButtplugClient) -> Result<bool, ButtplugClientError>
+{
+  println!("Sending commands");
+  
+    // Now that we know the message types for our connected device, we
+    // can send a message over! Seeing as we want to stick with the
+    // modern generic messages, we'll go with VibrateCmd.
+    //
+    // There's a couple of ways to send this message.
+    let test_client_device = &client.devices()[0];
+  
+    // We can use the convenience functions on ButtplugClientDevice to
+    // send the message. This version sets all of the motors on a
+    // vibrating device to the same speed.
+    test_client_device
+      .vibrate(&ScalarValueCommand::ScalarValue(1.0))
+      .await?;
+  
+    // If we wanted to just set one motor on and the other off, we could
+    // try this version that uses an array. It'll throw an exception if
+    // the array isn't the same size as the number of motors available as
+    // denoted by FeatureCount, though.
+    //
+    // You can get the vibrator count using the following code, though we
+    // know it's 2 so we don't really have to use it.
+    let vibrator_count = test_client_device
+      .vibrate_attributes()
+      .len();
+  
+    println!(
+      "{} has {} vibrators.",
+      test_client_device.name(),
+      vibrator_count,
+    );
+  
+    // Just set all of the vibrators to full speed.
+    if vibrator_count > 1 {
+      test_client_device
+        .vibrate(&ScalarValueCommand::ScalarValueVec(vec![1.0, 0.0]))
+        .await?;
+    } else {
+      println!("Device does not have > 1 vibrators, not running multiple vibrator test.");
+    }
+    Ok(true)
+    // wait_for_input().await;
+    // println!("Disconnecting");
+    // // And now we disconnect as usual.
+    // println!("Trying error");
+    // // If we try to send a command to a device after the client has
+    // // disconnected, we'll get an exception thrown.
+    // let vibrate_result = test_client_device
+    //   .vibrate(&ScalarValueCommand::ScalarValue(1.0))
+    //   .await;
+    // if let Err(ButtplugClientError::ButtplugConnectorError(error)) = vibrate_result {
+    //   println!("Tried to send after disconnection! Error: ");
+    //   println!("{}", error);
+    // }
 }
